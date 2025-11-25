03e0fc7ae62710b8034afddc9848da9e3f0704b3
ashenafi <ashenafiadola05@gmail.com>
2025-11-25 21:20:36 +0300

    added insertion function

--- linkedList.py
+++ linkedList.py
@@ -23,15 +23,41 @@ def deleteSpecificNode(head, nodeToDelete):
 
     return head
 
+def insertNewNode(head, new_node, position):
+    if position == 1:
+        new_node.next = head
+        return new_node
+    currentNode = head
+    i = 2
+    while currentNode.next and position != i:
+        currentNode = currentNode.next
+        i += 1
+    
+    nextnode = currentNode.next
+    currentNode.next = new_node
+    new_node.next = nextnode
+
+    return head
+
+
 node1 = Node(7)
 node2 = Node(11)
 node3 = Node(3)
 node4 = Node(2)
 node5 = Node(9)
+new_node = Node(94)
 
 node1.next = node2
 node2.next = node3
 node3.next = node4
 node4.next = node5
-
+print("before deletion")
+traverseList(node1)
+deleteSpecificNode(node1, node3)
+print('after deletion')
+traverseList(node1)
+print("before insertion")
+traverseList(node1)
+print("after insetion")
+node1 = insertNewNode(node1, new_node, 4)
 traverseList(node1)
