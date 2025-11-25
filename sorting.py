82f594b95f3641084762374100f1f7b60aa0b6dd
ashenafi <ashenafiadola05@gmail.com>
2025-11-25 15:17:23 +0300

    added selection sort

--- sorting.py
+++ sorting.py
@@ -7,3 +7,15 @@ def bubblesort(nums):
                 nums[j],nums[j+1] = nums[j+1],nums[j]
     print(nums)
 bubblesort(list1)
+
+list2 = [7, 1, 9, 4, 2, 8, 3, 6, 5]
+def selectionsort(nums):
+    for i in range(len(nums)-1):
+        minInd = i
+        for j in range(i+1, len(nums)):
+            if nums[j] < nums[minInd]:
+                minInd = j
+        a = nums.pop(minInd)
+        nums.insert(i,a)
+    print(nums)
+selectionsort(list2)
