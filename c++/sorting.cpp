f61f8dc55a1dd8eb2c999d5a6f04c348a7cb848b
ashenafi <ashenafiadola05@gmail.com>
2026-01-06 16:42:46 +0300

    implemented selection sort

--- c++/sorting.cpp
+++ c++/sorting.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int array[] = {34,45,23,56,23,44,35,77,26,3,2,5,43};
 int array1[] = {34,45,23,56,23,44,35,77,26,3,2,5,43};
 int array2[] = {34,45,23,56,23,44,35,77,26,3,2,5,43};
+int test[] = {1,2,3,4,5,6,7,8,9};
 
-void bubble_sort(){
-    for(int i = 0; i<13; i++){
-        for(int j = 0; j < 13; j++){
+void bubble_sort(int *array){
+    for(int i = 1; i<12; i++){
+        for(int j = 0; j < 13-i; j++){
             if(array[j] > array[j+1]){
                 int temp = array[j];
                 array[j] = array[j+1];
@@ -23,8 +24,26 @@ void bubble_sort(){
     cout<<endl;
 }
 
+void selection_sorting(int *array){
+    for(int i = 0; i < 13; i++){
+        for(int j = i+1; j < 13; j++){
+            if(array[j] < array[i]){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    cout<<endl;
+    for(int k = 0; k < 13; k++){
+        cout<<array[k]<<", ";
+    }
+    cout<<endl;
+}
+
 int main(){
 
-    bubble_sort();
+    bubble_sort(array1);
+    selection_sorting(array2);
     return 0;
 }
